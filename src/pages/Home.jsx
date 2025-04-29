236463d9d7d8cde374ef5a15edd242dff4254f42
Divyanshu Vats <divyanshuvats1006@gmail.com>
2025-04-29 13:32:05 +0530

    some change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,7 +41,7 @@ const Home = () => {
 
       <div className="max-w-3xl mx-auto text-center px-4 py-20">
         <h1 className="text-4xl font-bold text-purple-700 mb-4">
-          Welcome to the docker
+          Welcome to class
         </h1>
 
         {user ? (
